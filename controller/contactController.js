05be13321182a0ad8477801bed975b7b01cbd853
minhaz74692 <minhaz74692@gmail.com>
2024-02-14 23:31:23 +0600

    parse body json

--- controller/contactController.js
+++ controller/contactController.js
@@ -9,28 +9,29 @@ const getContacts = (req, res)=>{
 //@route Post /api/contacts
 //@access public
 const postContacts = (req, res)=>{
-    res.status(200).json({"message":`Post Without Id, contact`});
+    res.status(200).json({"message":`Post Without Id, ${req.params.id}`});
 };
 
 //@desc Post Contact
 //@route Post /api/contacts
 //@access public
 const createContact = (req, res)=>{
-    res.status(200).json({"message":`Post with Id, contact`});
+    res.status(200).json({"message":`Post with Id, ${req.params.id}`});
 };
 
 //@desc Put Contact
 //@route Put /api/contacts
 //@access public
 const updateContact = (req, res)=>{
-    res.status(200).json({"message":`Put with Id, contact`});
+    console.log(`The body is : ${req.body['email']}`);
+    res.status(200).json({"message":`Put with Id, ${req.params.id}`});
 };
 
 //@desc Delete Contact
 //@route Delete /api/contacts
 //@access public
 const deleteContact = (req, res)=>{
-    res.status(200).json({"message":`Delete with Id, contact`});
+    res.status(200).json({"message":`Delete with Id, ${req.params.id}`});
 };
 
 module.exports = {getContacts, postContacts, createContact, updateContact, deleteContact};
